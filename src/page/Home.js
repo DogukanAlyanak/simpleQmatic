39d375fa87502eb0cec909933f8f8117cd87cb50
Doğukan ALYANAK <mail@dogukanalyanak.com>
2019-11-03 12:02:48 +0300

    v1.0.1 - project update

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Q-Matic</h1>
+                <hr />
+                <Link to = {`/`}>Home</Link><br/>
+                <Link to = {`rowscreen`}>Sıra Ekranı</Link><br/>
+                <Link to = {`addrowscreen`}>Sıra Al Ekranı</Link><br/>
+                <Link to = {`waitrowscreen`}>Bekleyenler</Link><br/>
+                <hr/>
+                <h3>Counter</h3>
+                <Link to = {`counter/1`}>Gişe 1 Ekranı</Link><br/>
             </div>
         )
     }
